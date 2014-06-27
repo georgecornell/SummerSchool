9abc9e42bb8c8bd9a3b675863d8dd5937bdc4117
georgecornell <valentingeorgescu@MBPVali.local>
2014-06-26 22:35:09 -0500

    Quadratic Equation - first assignment

---
+++ QuadraticEquation/solveEq2.py
@@ -0,0 +1,23 @@
+import sys
+import math, cmath
+
+try:
+  a = float(sys.argv[1])
+  b = float(sys.argv[2])
+  c = float(sys.argv[3])
+
+  d = b*b-4*a*c
+  x = -b/2*a
+  y = cmath.sqrt(d)/(2*a) if d < 0 else math.sqrt(d)/(2*a)
+
+  print str(x + y)
+  if (y):
+    print str(x - y)
+  
+except IndexError:
+  print 'Not enough arguments'
+except ValueError:
+  print 'Program arguments must be numbers' 
+except ZeroDivisionError:
+  print 'first argument(a) may not be zero'
+
